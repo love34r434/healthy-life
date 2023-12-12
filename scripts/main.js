0cb2de2a391c80ba5095549616f5901d79a83ba1
CherryLove <vyshneva.liubov@gmail.com>
2023-12-12 11:15:44 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,10 @@
 // loading content
-
 async function loadContent(pageId, pageTitle, headerTitle) {
-    const response = await fetch(`pages/${pageId}.html`).then(response => response.text());
+    const content = await fetch(`pages/${pageId}.html`).then(response => response.text());
     document.getElementById('content').innerHTML = content;
 }
 
-//default load pages/index.html
+// default load pages/index.html
 document.addEventListener('DOMContentLoaded', function() {
     loadContent('index');
-})
+});
