b1824c919d0d072204075ab542c479586a1bb1f6
CherryLove <vyshneva.liubov@gmail.com>
2023-12-12 11:11:58 +0000

    Load content

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 // loading content
 
 async function loadContent(pageId, pageTitle, headerTitle) {
-    const response = await fetch('pages/$pageId.html').then(response => response.text());
+    const response = await fetch('pages/${pageId}.html').then(response => response.text());
     document.getElementById('content').innerHTML = content;
 }
 
