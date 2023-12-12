941f490881c1668527699ecd17880fbf093ddfc0
CherryLove <vyshneva.liubov@gmail.com>
2023-12-12 13:17:42 +0000

    update main.js

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 // loading content
 async function loadContent(pageId, pageTitle, headerTitle) {
-    try {
-        const response = await fetch(`/pages/${pageId}.html`);
-        if (!response.ok) {
-            throw new Error(`Error loading page: ${response.statusText}`);
-        }
-        const content = await response.text();
-        document.getElementById('content').innerHTML = content;
-    } catch (error) {
-        console.error(error);
-    }
+    const url = pageId ? `/pages/${pageId}.html` : '/index.html';
+
+    fetch(url)
+        .then(response => response.text())
+        .then(content => {
+            document.getElementById('content').innerHTML = content;
+            // add title
+            document.getElementById('page-title').textContent = pageTitle || 'Healthy Life Support Community';
+            // add header
+            document.getElementById('header-title').textContent = headerTitle || '';
+
+            //changing url
+            window.history.pushState({ pageId: pageId}, null, pageId ? `?page=${pageId}` : '/');
+        })
+        .catch(error => console.error('Error fetching content:', error));
 }
+
+
+
+window.addEventListener('popstate', function (event) {
+    const pageId = event.state ? event.state.pageId : 'index';
+    loadContent(pageId, pageTitle, headerTitle);
+    // highlightCurrentPage(pageId);
+});
+
 
 // default load pages/index.html
 document.addEventListener('DOMContentLoaded', function() {
-    loadContent('index');
+    loadContent('index', 'Healthy Life Support Community', '');
 });
