d79388360e039cb38e6974415949ad5545821ea7
CherryLove <vyshneva.liubov@gmail.com>
2023-12-14 00:47:25 +0000

    form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,62 +1,59 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const accordions = document.querySelectorAll(".accordion");
 
-    //accordion and form validator
-    document.addEventListener("DOMContentLoaded", function () {
-      const accordion = document.querySelectorAll(".accordion");
+    accordions.forEach((currentAccordion) => {
+        currentAccordion.addEventListener("click", function () {
+            const targetId = this.getAttribute("data-target");
+            const targetForm = document.getElementById(targetId);
+            const otherForms = document.querySelectorAll(".contactForm:not(#" + targetId + ")");
 
-      accordion.forEach((accordion) => {
-        accordion.addEventListener("click", function () {
-          const targetId = this.getAttribute("data-target");
-          const targetForm = document.getElementById(targetId);
-          const otherForms = document.querySelectorAll(
-            ".contactForm:not(#" + targetId + ")"
-          );
+            //Toggle the visibility of the target form
+            if (targetForm.style.display === "block") {
+                targetForm.style.display = "none";
+                this.textContent = "Show Form";
+            } else {
+                targetForm.style.display = "block";
+                this.textContent = "Hide Form";
+            }
 
-          //Toggle the visibility of the target form
-          if (targetForm.style.display === "block") {
-            targetForm.style.display = "none";
-            this.textContent = "Show Form";
-          } else {
-            targetForm.style.display = "block";
-            this.textContent = "Hide Form";
-          }
-
-          //Hide other Forms
-          otherForms.forEach((form) => (form.style.display = "none"));
+            //Hide other Forms
+            otherForms.forEach((form) => (form.style.display = "none"));
         });
-      });
+    });
 
-      var acc = document.querySelector(".accordion");
-      var panel = document.querySelector(".panel");
+    var acc = document.querySelector(".accordion");
+    var panel = document.querySelector(".panel");
 
-      acc.addEventListener("click", function () {
+    acc.addEventListener("click", function () {
         panel.style.display =
-          panel.style.display === "block" ? "none" : "block";
-      });
+            panel.style.display === "block" ? "none" : "block";
+    });
 
-      var sendMessageForm = document.getElementById("sendMessageForm");
+    var sendMessageForm = document.getElementById("sendMessageForm");
 
-      sendMessageForm.addEventListener("input", function () {
+    sendMessageForm.addEventListener("input", function () {
         var isFormValid = sendMessageForm.checkValidity();
         sendMessageForm.querySelector('button[type="submit"]').disabled =
-          !isFormValid;
-      });
+            !isFormValid;
+    });
 
-      sendMessageForm.addEventListener("submit", function (event) {
+    sendMessageForm.addEventListener("submit", function (event) {
         event.preventDefault();
         alert("Form sent!");
         sendMessageForm.reset();
-      });
-      var membershipForm = document.getElementById("membershipForm");
+    });
 
-      membershipForm.addEventListener("input", function () {
+    var membershipForm = document.getElementById("membershipForm");
+
+    membershipForm.addEventListener("input", function () {
         var isFormValid = membershipForm.checkValidity();
         membershipForm.querySelector('button[type="submit"]').disabled =
-          !isFormValid;
-      });
-    
-      membershipForm.addEventListener("submit", function (event) {
+            !isFormValid;
+    });
+
+    membershipForm.addEventListener("submit", function (event) {
         event.preventDefault();
         alert("Membership Form sent!");
         membershipForm.reset();
-      });
     });
+});
