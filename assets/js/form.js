842fdd5e0930924c4c74da99f9c9159a83a662cf
CherryLove <vyshneva.liubov@gmail.com>
2023-12-14 01:09:02 +0000

    delete accordions

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,27 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const accordions = document.querySelectorAll(".accordion");
-
-    accordions.forEach((currentAccordion) => {
-        currentAccordion.addEventListener("click", function () {
-            const targetId = this.getAttribute("data-target");
-            const targetForm = document.getElementById(targetId);
-            const otherForms = document.querySelectorAll(".contactForm:not(#" + targetId + ")");
-
-            // Toggle the visibility of the target form
-            if (targetForm.style.display === "block") {
-                targetForm.style.display = "none";
-                this.textContent = "Show Form";
-            } else {
-                targetForm.style.display = "block";
-                this.textContent = "Hide Form";
-            }
-
-            // Hide other Forms
-            otherForms.forEach((form) => (form.style.display = "none"));
-        });
-    });
-
-    var sendMessageForm = document.getElementById("sendMessageForm");
+     var sendMessageForm = document.getElementById("sendMessageForm");
 
     sendMessageForm.addEventListener("input", function () {
         var isFormValid = sendMessageForm.checkValidity();
