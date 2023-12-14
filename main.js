763b1ac408ba059d6fc030ecd296b45b044b09d8
CherryLove <vyshneva.liubov@gmail.com>
2023-12-14 02:31:16 +0000

    Update main.js

--- main.js
+++ main.js
@@ -18,33 +18,9 @@ async function loadContent(pageId) {
         
     //load script for form and accordion
     if (pageId === 'contact') {
-        var sendMessageForm = document.getElementById("sendMessageForm");
-
-        sendMessageForm.addEventListener("input", function () {
-            console.log('input');
-            var isFormValid = sendMessageForm.checkValidity();
-            console.log(isFormValid);
-            sendMessageForm.querySelector('button[type="submit"]').disabled = !isFormValid;
-        });
-    
-        sendMessageForm.addEventListener("submit", function (event) {
-            event.preventDefault();
-            alert("Message sent!");
-            sendMessageForm.reset();
-        });
-    
-        var membershipForm = document.getElementById("membershipForm");
-    
-        membershipForm.addEventListener("input", function () {
-            var isFormValid = membershipForm.checkValidity();
-            membershipForm.querySelector('button[type="submit"]').disabled = !isFormValid;
-        });
-    
-        membershipForm.addEventListener("submit", function (event) {
-            event.preventDefault();
-            alert("Membership Form sent!");
-            membershipForm.reset();
-        });
+        const script = document.createElement('script');
+        script.src = 'assets/js/form.js';
+        document.getElementById('add-scripts').appendChild(script);
     }
 }
 
