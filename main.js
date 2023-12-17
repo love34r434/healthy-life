ce38bd9b561503a51ae036cee072c6a3a70b4883
CherryLove <vyshneva.liubov@gmail.com>
2023-12-17 08:25:43 +0000

    Update main.js

--- main.js
+++ main.js
@@ -6,42 +6,26 @@ async function loadContent(pageId) {
         .then(content => {
             document.getElementById('content').innerHTML = content;
 
-            //adding title
+            // adding title
             const pageTitleElement = document.getElementById('title-of-page');
             const pageTitle = pageTitleElement ? pageTitleElement.textContent : 'Healthy Life';
             document.getElementById('page-title').textContent = pageTitle;
 
-            //changing url
-            window.history.pushState({pageId: pageId}, null, pageId ? `?page=${pageId}` : '/');
+            // changing url
+            window.history.pushState({ pageId: pageId }, null, pageId ? `?page=${pageId}` : '/');
+
+            // load script for form and accordion if the page is 'contact'
+            if (pageId === 'contact') {
+                const script = document.createElement('script');
+                script.src = 'assets/js/form.js';
+                document.getElementById('add-scripts').appendChild(script);
+            }
         })
         .catch(error => console.error('Error fetching content:', error));
-        
-    //load script for form and accordion
-    if (pageId === 'contact') {
-        const script = document.createElement('script');
-        script.src = 'assets/js/form.js';
-        document.getElementById('add-scripts').appendChild(script);
-    }
 }
 
 window.addEventListener('popstate', function (event) {
     const pageId = event.state ? event.state.pageId : 'index';
     loadContent(pageId);
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    loadContent('index', 'Healthy Life');  //default load index.html
-
-    const menuToggle = document.querySelector('.menu-toggle');
-    const nav = document.querySelector('nav');
-
-    if (menuToggle && nav) {
-        menuToggle.addEventListener('click', function () {
-            if (window.getComputedStyle(nav).display !== 'none') {
-                nav.classList.toggle('active');
-            }
-        });
-    }     
-});
-
 
